d907a40b0c55a004155a940556a6a75c629a1610
GustyWinds24 <tgustis@hotmail.com>
2019-12-05 17:48:36 -0600

    Built prototype of GUI and have it be able to be viewed.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,10 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -10,11 +12,18 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			// Load the FXML document (we created with SceneBuilder)
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation( Main.class.getResource("../Main.fxml") );
+			
+			// Load the layout from the FXML and add it to the scene
+			AnchorPane layout = (AnchorPane) loader.load();				
+			Scene scene = new Scene( layout );
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
