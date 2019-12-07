519ef339dccef4b554bc2aae31da99d3ebe8d1b4
GustyWinds24 <tgustis@hotmail.com>
2019-12-07 15:57:03 -0600

    Light edits.

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			
-			// Load the FXML document (we created with SceneBuilder)
+			// Load the FXML document
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation( Main.class.getResource("../Main.fxml") );
 			
